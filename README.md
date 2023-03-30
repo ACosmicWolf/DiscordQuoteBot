59ed6452867b39de0f652fd7b07da152bd2a72d4
ACosmicWolf <cosmicwolf009@gmail.com>
2023-03-30 19:39:38 +0530

    .

---
+++ README.md
@@ -0,0 +1,31 @@
+# SIMPLE Discord Bot that gives a random quote or a joke
+
+# Dependencies
+
+`pycord`
+`discord`
+`requests`
+
+## Can be installed using following commands
+
+`pip install discord pycord requests`
+
+## Run the bot using
+
+`python main.py`
+
+## Add bot to your server using
+
+`https://discord.com/api/oauth2/authorize?client_id=CLIENT_ID&permissions=8&scope=bot`
+
+Replace CLIENT_ID with your client id
+
+# Commands
+
+`/quote` - Gives a random quote
+
+`/joke` - Gives a random joke
+
+# Note
+
+You need to add your token in main.py
---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import requests
+intents = discord.Intents.default()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
+
+# Slash commands
+
+
+@bot.slash_command(name='ping', description='Pong!!!!')
+async def _ping(ctx):
+    await ctx.respond('Pong!')
+
+
+@bot.slash_command(name='quote', description='Random Quote')
+async def _quote(ctx):
+    r = requests.get('https://api.quotable.io/random')
+    quote = r.json()['content']
+    author = r.json()['author']
+    await ctx.respond(quote+" ~ "+author)
+
+
+@bot.slash_command(name='joke', description='Random Joke')
+async def _joke(ctx):
+    r = requests.get('https://official-joke-api.appspot.com/random_joke')
+    setup = r.json()['setup']
+    punchline = r.json()['punchline']
+    await ctx.respond(f"{setup} \n || {punchline} ||")
+
+
+bot.run('PLACE_YOUR_TOKEN_HERE')
